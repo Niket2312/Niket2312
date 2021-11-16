5f78cc269b1368c5df4e9fe3c06ef1be5a8d0e94
Niket2312 <niket.d@prominentpixel.com>
2021-11-16 11:57:09 +0530

    a

---
+++ working_with_libraries/moment.js
@@ -0,0 +1,10 @@
+      let d=new Date();
+      let d1=new Date("2015-03-20")
+      document.getElementById("p1").innerHTML=d1
+      let d2=new Date("2015-03")
+      document.getElementById("p2").innerHTML=d2
+      let d3=new Date("2000");
+      document.getElementById("p3").innerHTML=d3      
+      let d4=new Date("Dec 23 2000")
+      document.getElementById("p4").innerHTML=d4
+      
